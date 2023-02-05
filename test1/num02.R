e7d27216f5fe709981480e75dedba0d42ef054a5
kingwhangzang <soyun011112@sookmyung.ac.kr>
2023-02-05 12:52:49 +0900

    num01

---
+++ test1/num02.R
@@ -0,0 +1,12 @@
+rm(list=ls())
+ls()
+a <- 10
+b = 20
+hap <- a + b
+cat(a, "+", b, "=", hap, "\n")
+ls()
+rm(hap)
+ls()
+
+a_list = 10
+a.so = 20
---
+++ test1/num03.R
@@ -0,0 +1,8 @@
+rm(list=ls())
+iv <- c(1,2,3)
+cv <- c('A', 'B', 'C')
+bv <- c(TRUE,FALSE)
+fv <- c(1,2,3,4)
+icv <- c(iv,cv,bv,fv)
+
+ls()
